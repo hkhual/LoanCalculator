1ab1d5fa2c51f0a52e4c2d4348bfb390b150486d
Hau Khual <haukhual08@gmail.com>
2019-05-26 20:57:31 -0700

    add privacy activity in android minifest file

---
+++ app/src/main/java/loan/haugualnam/loancalculator/Privacy.java
@@ -0,0 +1,16 @@
+package loan.haugualnam.loancalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Privacy extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.privacy_activity);
+
+    }
+
+
+}
