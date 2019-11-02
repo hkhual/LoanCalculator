aebf2df8dc5a24ec7fff8965d3686d3f413c12e0
Hau Khual <haukhual08@gmail.com>
2019-11-02 00:07:09 -0400

    migrated to AndroidX

--- app/src/main/java/loan/haukh/loancalculator/AboutPage.java
+++ app/src/main/java/loan/haukh/loancalculator/AboutPage.java
@@ -1,7 +1,7 @@
 package loan.haukh.loancalculator;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutPage extends AppCompatActivity {
 
